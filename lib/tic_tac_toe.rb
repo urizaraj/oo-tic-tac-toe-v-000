25533fc68b091ba0af31be18a57a4169369ae7ac
Alexander Urizar <urizaraj@gmail.com>
2018-02-08 20:05:08 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -21,3 +21,11 @@ class TicTacToe
     puts "-----------"
     puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
   end
+
+  def input_to_index(input)
+    input.to_i - 1
+  end
+
+  def move(index, token = 'X')
+    @board[index] = token
+  end
